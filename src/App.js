b6e6f7e91041c941f210a4a953d978a438438e4c
alexsmaldone <alexsmaldone@gmail.com>
2022-07-25 23:24:12 -0400

    Added nasdaq tickers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { stocks } from "./stocks";
 
 function App() {
+  const stockList = stocks.map((stock, i) => (
+    <tr key={i}>
+      <td>{stock}</td>
+      <td>Apple Inc.</td>
+      <td>$0.53</td>
+      <td>$35.56</td>
+      <td>20.0%</td>
+      <td>20.0%</td>
+      <td>20.0%</td>
+      <td>20.0%</td>
+    </tr>
+  ));
+
+  console.log(stocks);
   return (
     <div className="page-container">
+      <h1>Nasdaq-100 Stocks</h1>
       <div className="stock-table">
         <table>
           <thead>
             <tr>
               <th>Ticker</th>
               <th>Name</th>
-              <th>Price</th>
-              <th>Change</th>
-              <th>Change %</th>
-              <th>Volume</th>
-              <th>Market Cap</th>
+              <th>Market Cap ($B)</th>
+              <th>Last Close Price</th>
+              <th>YTD % Change</th>
+              <th>3-Month % Change</th>
+              <th>6-Month % Change</th>
+              <th>12-Month % Change</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>AAPL</td>
-              <td>Apple Inc.</td>
-              <td>$AAPL</td>
-              <td>$AAPL</td>
-              <td>$AAPL</td>
-              <td>$AAPL</td>
-              <td>$AAPL</td>
-            </tr>
-          </tbody>
+          <tbody>{stockList}</tbody>
         </table>
       </div>
     </div>
